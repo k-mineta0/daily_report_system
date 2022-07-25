eaefcf425d7d10b0ae70d79de9a38dac6e404121
mineta <mineta@localhost>
2022-07-26 06:45:41 +0900

    LIKEモデル　LIKEVIEWモデル作成

--- src/main/java/actions/views/LikeConverter.java
+++ src/main/java/actions/views/LikeConverter.java
@@ -11,62 +11,62 @@ import models.Like;
  */
 public class LikeConverter {
 
-    /**
-     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
-     * @param rv LikeViewのインスタンス
-     * @return Likeのインスタンス
-     */
-    public static Like toModel(LikeView lv) {
-        return new Like(
-                lv.getId(),
-                ReportConverter.toModel(lv.getReport()),
-                EmployeeConverter.toModel(lv.getEmployee()));
+	/**
+	 * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+	 * @param rv LikeViewのインスタンス
+	 * @return Likeのインスタンス
+	 */
+	public static Like toModel(LikeView lv) {
+		return new Like(
+				lv.getId(),
+				ReportConverter.toModel(lv.getReport()),
+				EmployeeConverter.toModel(lv.getEmployee()));
 
-    }
+	}
 
-    /**
-     * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
-     * @param l Likeのインスタンス
-     * @return LikeViewのインスタンス
-     */
-    public static LikeView toView(Like l) {
+	/**
+	 * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+	 * @param l Likeのインスタンス
+	 * @return LikeViewのインスタンス
+	 */
+	public static LikeView toView(Like l) {
 
-        if (l == null) {
-            return null;
-        }
+		if (l == null) {
+			return null;
+		}
 
-        return new LikeView(
-        l.getId(),
-        ReportConverter.toView(l.getReport()),
-        EmployeeConverter.toView(l.getEmployee()));
+		return new LikeView(
+				l.getId(),
+				ReportConverter.toView(l.getReport()),
+				EmployeeConverter.toView(l.getEmployee()));
 
-    }
+	}
 
-    /**
-     * DTOモデルのリストからViewモデルのリストを作成する
-     * @param list DTOモデルのリスト
-     * @return Viewモデルのリスト
-     */
-    public static List<LikeView> toViewList(List<Like> list) {
-        List<LikeView> evs = new ArrayList<>();
+	/**
+	 * DTOモデルのリストからViewモデルのリストを作成する
+	 * @param list DTOモデルのリスト
+	 * @return Viewモデルのリスト
+	 */
+	public static List<LikeView> toViewList(List<Like> list) {
+		List<LikeView> evs = new ArrayList<>();
 
-        for (Like l : list) {
-            evs.add(toView(l));
-        }
+		for (Like l : list) {
+			evs.add(toView(l));
+		}
 
-        return evs;
-    }
+		return evs;
+	}
 
-    /**
-     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
-     * @param l DTOモデル(コピー先)
-     * @param lv Viewモデル(コピー元)
-     */
-    public static void copyViewToModel(Like l, LikeView lv) {
-        l.setId(lv.getId());
-        l.setReport(ReportConverter.toModel(lv.getReport()));
-        l.setEmployee(EmployeeConverter.toModel(lv.getEmployee()));
+	/**
+	 * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+	 * @param l DTOモデル(コピー先)
+	 * @param lv Viewモデル(コピー元)
+	 */
+	public static void copyViewToModel(Like l, LikeView lv) {
+		l.setId(lv.getId());
+		l.setReport(ReportConverter.toModel(lv.getReport()));
+		l.setEmployee(EmployeeConverter.toModel(lv.getEmployee()));
 
-    }
+	}
 
 }
